4bf903669a8a11b4d08efe5de17839facda4551c
avi1234 <avi1234@gmail.com>
2023-11-24 12:14:35 +0200

    hi

---
+++ guy.js
@@ -0,0 +1,6 @@
+console.log('hi Guy, erev tov')
+
+function hello() {
+    console.log('someone called the hello function')
+    document.getElementById('myTextDivId').innerHTML = 'text was changed! and random number is ' + Math.random()
+}
